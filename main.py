1eabbb951a71820ec4a935478a0f8151b4e3b919
notii <notii@bts.fail>
2021-10-26 00:16:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import requests
 with open("accounts.txt") as f:
     accounts = f.read()
     f.close()
-    for account in accounts.strip().split("\n"):    
-        r = requests.get(f"https://webapi.depop.com/api/v1/auth/checkusername/{account}")
-        if r.status_code == 200:
-            print(f"[Depop] Claimable: {account}")
-            f = open(f'out.txt', "a")
-            f.write(f"{account}\n")
-            f.close()
-        else:
-            print(f"[Depop] Unclaimable: {account} | Reason: {r.json()['description']}")
+    for account in accounts.strip().split("\n"):
+        if account.len >= 3:
+            r = requests.get(f"https://webapi.depop.com/api/v1/auth/checkusername/{account}")
+            if r.status_code == 200:
+                print(f"[Depop] Claimable: {account}")
+                f = open(f'out.txt', "a")
+                f.write(f"{account}\n")
+                f.close()
+            else:
+                print(f"[Depop] Unclaimable: {account} | Reason: {r.json()['description']}")
+        else: 
+            print(f"[Depop] Unclaimable: {account} | Reason: Username length is less than 3")
